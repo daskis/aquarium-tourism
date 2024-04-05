d56c9c2b595a653c2d74099ebb497e134b7fe68f
yurasankin <yurasankin@yandex.ru>
2024-04-05 16:20:50 +0300

    add swagger

--- back/.venv/bin/activate.csh
+++ back/.venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/exiz/hack/back4rdn/.venv"
+setenv VIRTUAL_ENV "/home/exiz/inprogress/back/.venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
--- back/myproject/myproject/settings.py
+++ back/myproject/myproject/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # extensions
+    'drf_yasg',
     'rest_framework_simplejwt',
     'rest_framework',
     'corsheaders',
@@ -69,7 +70,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
