96847d1a12d9fe4a014c31e446a7fd2b7035387b
Далер <101361201+DarkSteelD@users.noreply.github.com>
2024-04-06 08:31:28 +0300

    Фдьщые вщту

--- createdb.py
+++ createdb.py
@@ -1,20 +1,44 @@
 import sqlite3
 
-def create_db():
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        user_id INTEGER PRIMARY KEY,
-        username TEXT NOT NULL,
-        class TEXT NOT NULL,
-        image_path TEXT NOT NULL,
-        speed INTEGER DEFAULT 1,
-        cunning INTEGER DEFAULT 1,
-        luck INTEGER DEFAULT 1
+def create_databases():
+    # Создание базы данных пользователей
+    # users_conn = sqlite3.connect('users.db')
+    # users_cursor = users_conn.cursor()
+    # users_cursor.execute('''
+    # CREATE TABLE IF NOT EXISTS users (
+    #     user_id INTEGER PRIMARY KEY,
+    #     username TEXT NOT NULL,
+    #     class TEXT NOT NULL,
+    #     image_path TEXT NOT NULL,
+    #     speed INTEGER DEFAULT 1,
+    #     cunning INTEGER DEFAULT 1,
+    #     luck INTEGER DEFAULT 1
+    # );
+    # ''')
+    # users_conn.commit()
+    # users_conn.close()
+
+    # Создание базы данных квестов и спецпредложений
+    quests_offers_conn = sqlite3.connect('users.db')
+    quests_offers_cursor = quests_offers_conn.cursor()
+    quests_offers_cursor.execute('''
+    CREATE TABLE IF NOT EXISTS quests (
+        quest_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT NOT NULL,
+        image_path TEXT
     );
     ''')
-    conn.commit()
-    conn.close()
+    quests_offers_cursor.execute('''
+    CREATE TABLE IF NOT EXISTS offers (
+        offer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT NOT NULL,
+        discount TEXT,
+        image_path TEXT
+    );
+    ''')
+    quests_offers_conn.commit()
+    quests_offers_conn.close()
 
-create_db()
+create_databases()
