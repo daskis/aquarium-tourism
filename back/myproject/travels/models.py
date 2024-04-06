85d9426f15fbbafe17383e16568724de4fd15b2a
yurasankin <yurasankin@yandex.ru>
2024-04-06 17:53:25 +0300

    to show

--- back/myproject/travels/models.py
+++ back/myproject/travels/models.py
@@ -2,6 +2,17 @@ from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
+STATUS_FOR_LOCATIONS = {
+    "done": "DONE",
+    "not_active": "NOT_ACTIVE",
+    "pending": "PENDING"
+}
+STATUS_FOR_STEPS = {
+    "done": "DONE",
+    "not_active": "NOT_ACTIVE",
+    "pending": "PENDING"
+
+}
 
 
 class Locations(models.Model):
@@ -9,9 +20,8 @@ class Locations(models.Model):
     description = models.TextField(max_length=200)
     price = models.FloatField()
     city = models.CharField(max_length=20)
-    status = models.CharField(max_length=20)
-    steps = models.ForeignKey('Steps',on_delete=models.CASCADE, null=True, blank=True)
-
+    status = models.CharField(choices=STATUS_FOR_LOCATIONS, max_length=10, default="NOT_ACTIVE")
+    steps = models.ForeignKey('Steps', on_delete=models.CASCADE, null=True, blank=True)
 
 
 class ImageLocation(models.Model):
@@ -19,18 +29,27 @@ class ImageLocation(models.Model):
     image = models.ForeignKey('Locations', on_delete=models.CASCADE, to_field='id')
 
 
-
 class Steps(models.Model):
     title = models.CharField(max_length=20)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images/steps',null=True, blank=True)
     array_interval = models.JSONField(default=dict)
+    coordinates = models.JSONField(default=dict)
+    status = models.CharField(choices=STATUS_FOR_STEPS, max_length=10, default="NOT_ACTIVE")
 
     def clean(self):
-        if self.array_interval and ('time_started' not in self.array_interval or 'time_ended' not in self.array_interval):
+        if self.array_interval and (
+                'time_started' not in self.array_interval or 'time_ended' not in self.array_interval):
             raise ValidationError("JSON data must contain keys 'time_started' and 'time_ended'.")
+        if self.array_interval and (
+                'latitude' not in self.coordinates or 'longitude' not in self.coordinates):
+            raise ValidationError("JSON data must contain keys 'latitude' and 'longitude'.")
+        print(self.coordinates['latitude'])
+        if not (-90 < self.coordinates['latitude'] < 90):
+            raise ValidationError("out if range for 'latitude'")
+        if not (-180 < self.coordinates['longitude'] < 180):
+            raise ValidationError("out if range for 'longitude'")
+
 
     def save(self, *args, **kwargs):
         self.full_clean()
         super().save(*args, **kwargs)
-
-
